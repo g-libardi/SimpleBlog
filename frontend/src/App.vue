6e59bb55b8adee16ead54eddb7e8f23b9788c6f8
Guilherme Libardi <guilhermelibardi@hotmail.com>
2023-12-02 14:22:02 -0300

    fix: app.vue logged in access

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,14 +2,14 @@
 import Feed from './components/Feed.vue';
 import Login from './components/Login.vue';
 // import Router from './components/Router.vue';
-import { logged_in } from './store';
+import { authData } from './store';
 
 </script>
 
 <template>
   <div class="w-screen min-h-screen bg-slate-600 text-white flex justify-center">
     <Feed/>
-    <Login v-if="!logged_in"/>
+    <Login v-if="!authData.logged_in"/>
   </div>
 </template>
 
