7d44e705551f4b34296d060f32c47650e54c9b47
Guilherme Libardi <guilhermelibardi@hotmail.com>
2023-12-05 10:47:13 -0300

    add: user specific related routes

--- backend/server.py
+++ backend/server.py
@@ -6,6 +6,11 @@ import db
 app = Flask(__name__)
 CORS(app)
 
+def auth(username, token):
+    if auth.auth(username, token):
+        user_info = db.User.get(username, 'username', 'name')
+        return jsonify(user_info), 200
+
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
@@ -58,6 +63,53 @@ def posts():
         return jsonify({'error': 'No posts found'}), 404
     return jsonify(posts), 200
 
+@app.route('/u/<username>', methods=['GET'])
+def user(username):
+    user = db.User.get(username, 'username', 'name')
+    if user is None:
+        return jsonify({'error': 'User not found'}), 404
+    return jsonify(user), 200
+
+@app.route('/u/<username>/followers', methods=['GET'])
+def followers(username):
+    followers = db.User.get_followers(username)
+    if followers is None:
+        return jsonify({'error': 'User not found'}), 404
+    return jsonify(followers), 200
+
+@app.route('/u/<username>/following', methods=['GET'])
+def following(username):
+    following = db.User.get_following(username)
+    if following is None:
+        return jsonify({'error': 'User not found'}), 404
+    return jsonify(following), 200
+
+@app.route('/u/<username>/follow', methods=['POST'])
+def follow(username):
+    data = request.get_json()
+    follower_username = data['follower_username']
+    token = data['token']
+    if auth.auth(username, token):
+        db.User.follow(username, follower_username)
+        return jsonify({'message': 'User followed successfully'}), 200
+    return jsonify({'error': 'Invalid token'}), 401
+
+@app.route('/u/<username>/unfollow', methods=['POST'])
+def unfollow(username):
+    data = request.get_json()
+    current_user = data['auth']['username']
+    token = data['token']
+    if auth.auth(username, token):
+        db.User.unfollow(username, current_user)
+        return jsonify({'message': 'User unfollowed successfully'}), 200
+    return jsonify({'error': 'Invalid token'}), 401
+
+@app.route('/u/<username>/posts', methods=['GET'])
+def user_posts(username):
+    posts = db.Post.get_user_posts(username)
+    if posts is None:
+        return jsonify({'error': 'User not found'}), 404
+    return jsonify({'posts': posts}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
